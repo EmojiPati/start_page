69406d5c58fb3e55b68eab59933ece224a4000e0
EmojiPati <bestami355karakaya@gmail.com>
2023-12-13 02:48:11 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import React from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import "@/styles/globals.css"
 import { SettingsProvider } from "@/context/settings"
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 function fallbackRender({ error, resetErrorBoundary }) {
